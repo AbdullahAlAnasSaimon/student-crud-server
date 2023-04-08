60a4e1c19e3232a503c68ce03309a63b8750e08f
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-04-08 15:54:37 +0600

    compoleted curd operation

--- index.js
+++ index.js
@@ -46,13 +46,20 @@ async function run() {
 
     app.put('/student/:id', async(req, res) =>{
       const id = req.params.id;
-      const filter = {_id: ObjectId(id)};
+      const filter = {_id: new ObjectId(id)};
       const updateDoc = {
         $set: {
           reg: 1111
         }
       }
       const result = await studentsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    app.delete('/student/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await studentsCollection.deleteOne(query);
       res.send(result);
     })
 
